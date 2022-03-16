db43f83796ed2a05d69972c07c49546f433435ab
James Kayik Choi <62376291+SirPhysics@users.noreply.github.com>
2022-03-16 14:04:03 +1100

    Create Day-3.py

---
+++ Day-3.py
@@ -0,0 +1,39 @@
+/* Task
+Given an integer,
+
+, perform the following conditional actions:
+
+    If 
+
+is odd, print Weird
+If
+is even and in the inclusive range of to
+, print Not Weird
+If
+is even and in the inclusive range of to
+, print Weird
+If
+is even and greater than , print Not Weird
+*/
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    
+if N % 2 == 1:
+    print('Weird')
+    
+elif N % 2 == 0 and N >= 2 and N <= 5:
+        print('Not Weird')
+
+elif N % 2 == 0 and N >=6 and N<=20:
+    print('Weird')
+        
+elif N > 20:
+    print('Not Weird')
